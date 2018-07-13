b8ede663c2124dbd21c4c86114fc63a3eb01901f
MM00491424 <MM00491424@INNOSZPC02189.ATT.TechMahindra.com>
2018-07-14 00:03:09 +0530

    first Component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,19 @@
-console.log("************react start*****************");
+import React from "react";
+import { render } from "react-dom";
+
+
+class FirstComponent extends React.Component{
+	render() {
+		return(
+				<div className="container">
+				<div className="row">
+				<div className="col-xs-10 col-xs-offset-1">
+				<h1>Hello World!!</h1>
+				</div>
+				</div>
+				</div>
+		);
+	} 
+}
+
+render(<FirstComponent/>, window.document.getElementById("app"));
