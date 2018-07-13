fa8acbf1ae3ee71207dd4df660c5247240490f65
Mayank <mittalmayank10@gmail.com>
2018-07-14 02:55:22 +0530

    updating pkgjson with prop-types and learn how to make components and use of props.

--- src/app/index.js
+++ src/app/index.js
@@ -1,16 +1,29 @@
 import React from "react";
 import { render } from "react-dom";
 
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
 
 class FirstComponent extends React.Component{
 	render() {
+		var user = {
+			name : "Pulkit",
+			hobbies : ["cricket", "mobile"]
+		};
 		return(
 				<div className="container">
-				<div className="row">
-				<div className="col-xs-10 col-xs-offset-1">
-				<h1>Hello World!!</h1>
-				</div>
-				</div>
+					<div className="row">
+						<div className="col-xs-10 col-xs-offset-1">
+							<Header/>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-xs-10 col-xs-offset-1">
+							<Home name={"Mayank"} age={22} user={user}>
+								<p>This is a para in Home component</p>
+							</Home>
+						</div>
+					</div>
 				</div>
 		);
 	} 
